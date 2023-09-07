4a35dec6ef87178b8affb2100082d94b1e0089ee
Yaroslav Babalich <y.babalich@gmail.com>
2023-09-07 22:40:47 +0300

    + uiColor hex string initialization

--- Sources/AppKit/Extensions/UIColor+Utilities.swift
+++ Sources/AppKit/Extensions/UIColor+Utilities.swift
@@ -33,6 +33,28 @@ public extension UIColor {
         self.init(from: (hex >> 16) & 0xff, green: (hex >> 8) & 0xff, blue: hex & 0xff, alpha: alpha)
     }
 
+    convenience init?(hexString: String) {
+        var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return nil
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
     var hexCodeString: String {
         var r:CGFloat = 0
         var g:CGFloat = 0
